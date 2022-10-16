3bbf6374cc006dc2fee0be283c547ee2bee5df6a
Vojtech Cizek <cizek.vojtech@gmail.com>
2022-10-16 21:54:29 +0200

    Initial commit

---
+++ src/Exceptions.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class MyException_PlnaNadrz extends RuntimeException {
+    public MyException_PlnaNadrz(String str) {
+        super(str);
+    }
+}
+
+public class MyException_PlnaNadrz extends RuntimeException {
+    public MyException_PlnaNadrz(String str) {
+        super(str);
+    }
+}
